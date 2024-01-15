02059a01388ced4411257b0f372be801f3ad5645
Anirudh Dattu <anirudh.dattu.22cse@bmu.edu.in>
2024-01-15 15:51:12 +0530

    footer and navbar update

--- fest-website/src/App.jsx
+++ fest-website/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import Navbar from './components/navbar'
 import Eventfest from './components/eventsfest'
 import Footer from './components/footer'
-// import Homepage from './components/Homepage/homepage'
+import Homepage from './components/Homepage/homepage'
 
 
 
@@ -16,7 +14,7 @@ function App() {
   return (
     <>
      <Navbar/>
-     {/* <Homepage/> */}
+     <Homepage/>
      <Eventfest/>
      <Footer/>
     </>
