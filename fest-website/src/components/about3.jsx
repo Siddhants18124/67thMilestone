223141609d33369d0a29fd08108ddec59b43354f
Anirudh Dattu <anirudh.dattu.22cse@bmu.edu.in>
2024-01-16 20:42:41 +0530

    about page first 3 components

---
+++ fest-website/src/components/about3.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+const About3 = () => {
+  return (
+    <div className="xl:mx-72 xl:my-36 mx-6 mt-4">
+      {/* first row */}
+      <div class="grid grid-cols-1 gap-4 lg:grid-cols-5 lg:gap-4">
+        <div class="h-auto rounded-lg  lg:col-span-2 ">
+          <div className="mr-12 mt-1 text-wrap">
+            <h1 className="font-extrabold text-xl  ">Meet The Core</h1>
+            <p className=" text-justify  text-lg mt-3">
+              the one who have made this all possible and made our fest a grand
+              success
+            </p>
+          </div>
+        </div>
+        <div class="h-auto rounded-lg ">
+          <img
+            src="https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?cs=srgb&dl=pexels-wendy-wei-1190298.jpg&fm=jpg"
+            alt="Image 1"
+            className="w-full object-cover"
+          />
+        </div>
+        <div class="h-auto rounded-lg ">
+          <img
+            src="https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?cs=srgb&dl=pexels-wendy-wei-1190298.jpg&fm=jpg"
+            alt="Image 1"
+            className="w-full object-cover"
+          />
+        </div>
+        <div class="h-auto rounded-lg ">
+          <img
+            src="https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?cs=srgb&dl=pexels-wendy-wei-1190298.jpg&fm=jpg"
+            alt="Image 1"
+            className="w-full object-cover"
+          />
+        </div>
+      </div>
+
+      {/* second row  */}
+
+      <div class="grid grid-cols-1 gap-4 lg:grid-cols-5 lg:gap-4 mt-5">
+        <div class="h-auto rounded-lg "><img
+            src="https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?cs=srgb&dl=pexels-wendy-wei-1190298.jpg&fm=jpg"
+            alt="Image 1"
+            className="w-full object-cover"
+          /></div>
+        <div class="h-auto rounded-lg "><img
+            src="https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?cs=srgb&dl=pexels-wendy-wei-1190298.jpg&fm=jpg"
+            alt="Image 1"
+            className="w-full object-cover"
+          /></div>
+        <div class="h-auto rounded-lg "><img
+            src="https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?cs=srgb&dl=pexels-wendy-wei-1190298.jpg&fm=jpg"
+            alt="Image 1"
+            className="w-full object-cover"
+          /></div>
+        <div class="h-auto rounded-lg "><img
+            src="https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?cs=srgb&dl=pexels-wendy-wei-1190298.jpg&fm=jpg"
+            alt="Image 1"
+            className="w-full object-cover"
+          /></div>
+        <div class="h-auto rounded-lg "><img
+            src="https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?cs=srgb&dl=pexels-wendy-wei-1190298.jpg&fm=jpg"
+            alt="Image 1"
+            className="w-full object-cover"
+          /></div>
+      </div>
+
+      
+    </div>
+  );
+};
+
+export default About3;
