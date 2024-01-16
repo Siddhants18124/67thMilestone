de9d7fde2cbbeae603bdda10d43b3fb5cec57a82
unknown <abhayrajeshshah@gmail.com>
2024-01-17 01:05:27 +0530

    add events page

--- fest-website/src/App.jsx
+++ fest-website/src/App.jsx
@@ -1,41 +1,41 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import './App.css'
 
-import Navbar from './components/navbar'
-import Eventfest from './components/eventsfest'
-import Footer from './components/footer'
-import Homepage from './components/Homepage/homepage'
-import Event from './components/event'
-import Homesection2 from './components/homesection2/homesection2'
-import Homesection3 from './components/homesection3/homesection3'
-import Homesection4 from './components/homesection4/homesection4'
-import Aboutpage from './components/aboutpage/aboutpage'
-import About2 from './components/about2/about2'
-import About3 from './components/about3'
-import About4 from './components/about4'
-import About5 from './components/about5'
+import Navbar from "./components/navbar";
+import Eventfest from "./components/eventsfest";
+import Footer from "./components/footer";
+import Homepage from "./components/Homepage/homepage";
+import Event from "./components/event";
+import Homesection2 from "./components/homesection2/homesection2";
+import Homesection3 from "./components/homesection3/homesection3";
+import Homesection4 from "./components/homesection4/homesection4";
+import Aboutpage from "./components/aboutpage/aboutpage";
+import About2 from "./components/about2/about2";
+import About3 from "./components/about3";
+import About4 from "./components/about4";
+import About5 from "./components/about5";
 
+import Events from "./components/events";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <Navbar/>
-     <Aboutpage/>
-     {/* <About2/>  */}
-     {/* <About3/>  */}
-     {/* <About4/> */}
-     {/* <About5/> */}
-     {/* <Event/> */}
-     {/* <Homepage/> */}
-     {/* <Homesection2/> */}
-     {/* <Homesection3/> */}
-     {/* <Homesection4/> */}
-     {/* <Eventfest/> */}
-     {/* <Footer/> */}
+      <Events />
+      {/* <Navbar />
+      <Aboutpage /> */}
+      {/* <About2/>  */}
+      {/* <About3/>  */}
+      {/* <About4/> */}
+      {/* <About5/> */}
+      {/* <Event/> */}
+      {/* <Homepage/> */}
+      {/* <Homesection2/> */}
+      {/* <Homesection3/> */}
+      {/* <Homesection4/> */}
+      {/* <Eventfest/> */}
+      {/* <Footer/> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
