2185b5659ebab6b7734da569e0c2ed2463869d3f
unknown <abhayrajeshshah@gmail.com>
2024-01-18 20:43:44 +0530

    Events page remake

--- fest-website/src/pages/events/index.jsx
+++ fest-website/src/pages/events/index.jsx
@@ -11,30 +11,28 @@ import "./index.css";
 
 const Events = () => {
   gsap.registerPlugin(ScrollTrigger);
-
+  const bg = useRef();
   useEffect(() => {
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".anim1",
-        start: "top top",
-        end: "bottom bottom",
-        scrub: true,
-        immediateRender: false,
-      },
-    });
-    tl.to(".img1", { top: "0%", opacity: 0 }).to(".img2", {
-      top: "50%",
-      opacity: 1,
-    });
-    gsap.to(".bg", {
-      scrollTrigger: {
-        trigger: ".section2",
-        scrub: true,
-        immediateRender: false,
-        start: "top 75%",
-        end: "top top",
-      },
-      backgroundPositionY: "0%",
+    const items = Array.from(bg.current.children);
+    items.splice(0, 1);
+    console.log(items);
+    items.forEach((item, i) => {
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".anim1",
+          start: `${(100 / items.length) * i}% top`,
+          end: "+=100%",
+          scrub: true,
+          markers: true,
+          immediateRender: false,
+        },
+      });
+      if (i !== items.length - 1) {
+        tl.to(item, { top: "0%", opacity: 0 }).to(items[i + 1], {
+          top: "50%",
+          opacity: 1,
+        });
+      }
     });
   }, []);
 
@@ -42,43 +40,16 @@ const Events = () => {
     <div className="events">
       <Navbar />
       <div className="event-type anim1">
-        <div className="bg">
+        <div ref={bg} className="bg">
           <div className="overlay"></div>
           <img src={logo} alt="" className="img1" />
           <img src={culture} alt="" className="img2" />
+          <CardSection />
+          <p className="type">Technical Events</p>
+          <CardSection />
+          <p className="type">Management Events</p>
+          <CardSection />
         </div>
-      </div>
-      <div className="card-section section2">
-        <Card image="https://picsum.photos/200" text="Cultural" />
-        <Card image="https://picsum.photos/200" text="Cultural" />
-        <Card image="https://picsum.photos/200" text="Cultural" />
-        <Card image="https://picsum.photos/200" text="Cultural" />
-        <Card image="https://picsum.photos/200" text="Cultural" />
-        <Card image="https://picsum.photos/200" text="Cultural" />
-        <Card image="https://picsum.photos/200" text="Cultural" />
-        <Card image="https://picsum.photos/200" text="Cultural" />
-      </div>
-      <Parallax event={"Technical Events"} />
-      <div className="card-section">
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-      </div>
-      <Parallax event={"Management Events"} />
-      <div className="card-section">
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
-        <Card image="https://picsum.photos/200/200" text="Cultural" />
       </div>
       <Footer />
     </div>
@@ -96,40 +67,55 @@ const Card = ({ image, text }) => {
   );
 };
 
-const Parallax = ({ event }) => {
-  const bg = useRef();
-  const element = useRef();
-  useEffect(() => {
-    gsap.to(bg.current, {
-      scrollTrigger: {
-        trigger: element.current,
-        start: "top 50%",
-        end: "top top",
-        scrub: true,
-        immediateRender: false,
-      },
-      backgroundPositionY: "50%",
-    });
-    gsap.to(bg.current, {
-      scrollTrigger: {
-        trigger: element.current,
-        start: "bottom bottom",
-        end: "+=30%",
-        scrub: true,
-        immediateRender: false,
-      },
-      backgroundPositionY: "0%",
-    });
-  }, []);
+const CardSection = () => {
   return (
-    <div className="event-type anim2" ref={element}>
-      <div className="bg" ref={bg}>
-        <div className="overlay">
-          <p className="type">{event}</p>
-        </div>
-      </div>
+    <div className="card-section">
+      <Card image="https://picsum.photos/200/200" text="Cultural" />
+      <Card image="https://picsum.photos/200/200" text="Cultural" />
+      <Card image="https://picsum.photos/200/200" text="Cultural" />
+      <Card image="https://picsum.photos/200/200" text="Cultural" />
+      <Card image="https://picsum.photos/200/200" text="Cultural" />
+      <Card image="https://picsum.photos/200/200" text="Cultural" />
+      <Card image="https://picsum.photos/200/200" text="Cultural" />
+      <Card image="https://picsum.photos/200/200" text="Cultural" />
     </div>
   );
 };
+
+// const Parallax = ({ event }) => {
+//   const bg = useRef();
+//   const element = useRef();
+//   useEffect(() => {
+//     gsap.to(bg.current, {
+//       scrollTrigger: {
+//         trigger: element.current,
+//         start: "top 50%",
+//         end: "top top",
+//         scrub: true,
+//         immediateRender: false,
+//       },
+//       backgroundPositionY: "50%",
+//     });
+//     gsap.to(bg.current, {
+//       scrollTrigger: {
+//         trigger: element.current,
+//         start: "bottom bottom",
+//         end: "+=30%",
+//         scrub: true,
+//         immediateRender: false,
+//       },
+//       backgroundPositionY: "0%",
+//     });
+//   }, []);
+//   return (
+//     <div className="event-type anim2" ref={element}>
+//       <div className="bg" ref={bg}>
+//         <div className="overlay">
+//           <p className="type">{event}</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default Events;
