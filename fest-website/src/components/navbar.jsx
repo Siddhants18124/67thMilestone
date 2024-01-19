0ae4c0402a73dfb393a582a407d638edadaf8c1c
unknown <abhayrajeshshah@gmail.com>
2024-01-19 18:07:52 +0530

    white bg, black text nav

--- fest-website/src/components/navbar.jsx
+++ fest-website/src/components/navbar.jsx
@@ -12,24 +12,16 @@ const Navbar = ({ black }) => {
   };
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-transparent p-4">
+    <nav className="fixed shadow-#333 shadow-md bg-white top-0 left-0 right-0 z-50 p-4">
       <div className="flex  flex-col lg:flex-row lg:items-center lg:justify-between">
         <div className="flex items-center justify-between">
-          <div>
-            {black ? (
-              <img src={blackLogo} alt="Logo" className="h-12" />
-            ) : (
-              <img src={logoImage} alt="Logo" className="h-12" />
-            )}
+          <div className="px-4">
+            <img src={blackLogo} alt="Logo" className="h-12" />
           </div>
           <div className="lg:hidden">
             <button
               onClick={toggleMenu}
-              className={
-                black
-                  ? "text-black focus:outline-none"
-                  : "text-white focus:outline-none"
-              }
+              className="text-black focus:outline-none"
             >
               &#9776;
             </button>
@@ -37,47 +29,25 @@ const Navbar = ({ black }) => {
         </div>
         <div
           className={`lg:flex ${
-            isMenuOpen ? "flex flex-col items-end" : "hidden"
+            isMenuOpen ? "flex flex-col items-start py-4" : "hidden"
           } lg:items-center`}
         >
-          <Link
-            to="/"
-            className={black ? "text-black px-4 py-2" : "text-white px-4 py-2"}
-          >
+          <Link to="/" className="text-black px-4 py-2">
             Home
           </Link>
-          <Link
-            to="/about"
-            className={black ? "text-black px-4 py-2" : "text-white px-4 py-2"}
-          >
+          <Link to="/about" className="text-black px-4 py-2">
             About
           </Link>
-          <Link
-            to="/contact"
-            className={black ? "text-black px-4 py-2" : "text-white px-4 py-2"}
-          >
+          <Link to="/contact" className="text-black px-4 py-2">
             Contact
           </Link>
-          <Link
-            to="/events"
-            className={black ? "text-black px-4 py-2" : "text-white px-4 py-2"}
-          >
+          <Link to="/events" className="text-black px-4 py-2">
             Events
           </Link>
-          <Link
-            to="/sponsors"
-            className={black ? "text-black px-4 py-2" : "text-white px-4 py-2"}
-          >
+          <Link to="/sponsors" className="text-black px-4 py-2">
             Sponsors
           </Link>
-          <Link
-            to="#"
-            className={
-              black
-                ? "text-black px-4 py-2 font-bold"
-                : "text-white px-4 py-2 font-bold"
-            }
-          >
+          <Link to="#" className="text-black px-4 py-2">
             Register
           </Link>
         </div>
