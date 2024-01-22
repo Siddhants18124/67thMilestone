6e16d516d11dd5bc41509853ffa3eeec7e0a0f5d
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-22 08:40:36 +0530

    Fix the center div for the navbar due to it being fixed.

--- fest-website-ts/src/components/Navbar.tsx
+++ fest-website-ts/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import NavbarLink from "./NavbarLink";
 
 function Navbar() {
   return (
-    <nav className="fixed z-20 w-full p-1 py-4 mobile_l:p-4 text-white flex flex-col justify-center gap-2 items-center md:flex-row md:justify-between md:px-6 lg:w-5/6 lg:mx-auto">
+    <nav className="absolute z-20 w-full p-1 py-4 mobile_l:p-4 text-white flex flex-col justify-center gap-2 items-center md:flex-row md:justify-between md:px-6 lg:w-5/6 left-1/2 -translate-x-1/2">
       <img src={NavBarImg} alt="Logo" className="h-16" />
       <ul className="flex flex-row gap-2 sm:gap-4 justify-center items-center text-sm mobile_m:text-lg mobile_l:text-xl">
         <NavbarLink name="Home" link="/" />
