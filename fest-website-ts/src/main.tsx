75b7292f56b3a7603ede157c58f134c2757a4207
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-22 10:07:13 +0530

    A basic loading page added.

--- fest-website-ts/src/main.tsx
+++ fest-website-ts/src/main.tsx
@@ -1,10 +1,15 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import LoadingPage from "./pages/Loading";
+const App = lazy(() => import("./App"));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <Suspense fallback={<LoadingPage />}>
+      <App />
+    </Suspense>
   </BrowserRouter>,
 );
