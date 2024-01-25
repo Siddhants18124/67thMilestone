cd2717ac8fa45414f44805f9a0372b1ed5f5572a
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-25 13:15:45 +0530

    min-h to 600px in the events page

--- fest-website-ts/src/pages/Events.tsx
+++ fest-website-ts/src/pages/Events.tsx
@@ -3,7 +3,7 @@ import EventsPageGrid from "../components/EventsPageGrid";
 
 function EventsPage() {
   return (
-    <section className="min-h-[550px] pt-32">
+    <section className="min-h-[600px] pt-32">
       <div className="my-4 grid gap-5 w-full place-content-center grid-cols-1 px-4 sm:w-11/12 sm:mx-auto sm:grid-cols-2">
         <EventsPageGrid
           url="/events/67milestone"
