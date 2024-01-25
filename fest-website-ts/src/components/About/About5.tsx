b35f230d38f2db36b3c6f28907ce28bf22bb5f6e
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-25 12:59:04 +0530

    About page completed.

--- fest-website-ts/src/components/About/About5.tsx
+++ fest-website-ts/src/components/About/About5.tsx
@@ -2,7 +2,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
-const About5 = ({ duration }) => {
+const About5 = ({ duration }: { duration: number }) => {
   const settings = {
     dots: false,
     infinite: true,
--- fest-website-ts/src/pages/About.tsx
+++ fest-website-ts/src/pages/About.tsx
@@ -2,6 +2,7 @@ import About1 from "../components/About/About1";
 import About2 from "../components/About/About2";
 import About3 from "../components/About/About3";
 import About4 from "../components/About/About4";
+import About5 from "../components/About/About5";
 
 function About() {
   return (
@@ -10,6 +11,7 @@ function About() {
       <About2 />
       <About3 />
       <About4 />
+      <About5 duration={5000} />
     </>
   );
 }
