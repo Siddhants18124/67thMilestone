740bfb6869203389820c45ace6d362dc39673f6e
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-30 17:50:30 +0530

    Fix spacing in navbar

--- fest-website-ts/src/components/Navbar.tsx
+++ fest-website-ts/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="absolute z-50 w-full p-1 py-4 mobile_l:p-4 text-white flex flex-col justify-center gap-2 items-center md:flex-row md:justify-between md:px-6 lg:w-11/12 left-1/2 -translate-x-1/2">
       <img src={Logo} alt="Logo" className="h-16" />
-      <ul className="flex flex-row gap-2 sm:gap-4 justify-center items-center text-sm mobile_m:text-lg mobile_l:text-xl">
+      <ul className="flex flex-row gap-4 lg:gap-16 sm:gap-4 justify-center items-center text-sm mobile_m:text-lg mobile_l:text-xl">
         <NavbarLink name="Home" link="/" />
         <NavbarLink name="About" link="/about" />
         <NavbarLink name="Events" link="/events" />
