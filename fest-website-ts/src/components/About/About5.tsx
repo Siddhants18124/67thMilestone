5710b20d6a37543481f6a5843bc112b759e4814d
unknown <abhayrajeshshah@gmail.com>
2024-02-06 13:34:14 +0530

    fix import

--- fest-website-ts/src/components/About/About5.tsx
+++ fest-website-ts/src/components/About/About5.tsx
@@ -1,34 +1,33 @@
 import "./About5.css";
-import c1 from "../../assets/cr/c1.jpg"
-import c2 from "../../assets/cr/c2.jpg"
-import c3 from "../../assets/cr/c3.jpg"
-import c4 from "../../assets/cr/c4.jpg"
-import c5 from "../../assets/cr/c5.jpg"
-import c6 from "../../assets/cr/c6.jpg"
-import c7 from "../../assets/cr/c7.jpg"
-import c8 from "../../assets/cr/c8.jpg"
-import c9 from "../../assets/cr/c9.jpg"
-import c10 from "../../assets/cr/c10.jpg"
-import c11 from "../../assets/cr/c11.jpg"
-import c12 from "../../assets/cr/c12.jpg"
-import c13 from "../../assets/cr/c13.jpg"
-import c14 from "../../assets/cr/c14.jpg"
+import c1 from "../../assets/cr/c1.png";
+import c2 from "../../assets/cr/c2.jpg";
+import c3 from "../../assets/cr/c3.jpg";
+import c4 from "../../assets/cr/c4.jpg";
+import c5 from "../../assets/cr/c5.jpg";
+import c6 from "../../assets/cr/c6.jpg";
+import c7 from "../../assets/cr/c7.jpg";
+import c8 from "../../assets/cr/c8.jpg";
+import c9 from "../../assets/cr/c9.jpg";
+import c10 from "../../assets/cr/c10.jpg";
+import c11 from "../../assets/cr/c11.jpg";
+import c12 from "../../assets/cr/c12.jpg";
+import c13 from "../../assets/cr/c13.jpg";
+import c14 from "../../assets/cr/c14.jpg";
 
-import a1 from "../../assets/cr2/a1.jpg"
-import a2 from "../../assets/cr2/a2.jpg"
-import a3 from "../../assets/cr2/a3.jpg"
-import a4 from "../../assets/cr2/a4.jpg"
-import a5 from "../../assets/cr2/a5.jpg"
-import a6 from "../../assets/cr2/a6.jpg"
-import a7 from "../../assets/cr2/a7.jpg"
-import a8 from "../../assets/cr2/a8.jpg"
-import a9 from "../../assets/cr2/a9.jpg"
-import a10 from "../../assets/cr2/a10.jpg"
-import a11 from "../../assets/cr2/a11.jpg"
-import a12 from "../../assets/cr2/a12.jpg"
-import a13 from "../../assets/cr2/a13.jpg"
-import a14 from "../../assets/cr2/a14.jpg"
-
+import a1 from "../../assets/cr2/a1.jpg";
+import a2 from "../../assets/cr2/a2.jpg";
+import a3 from "../../assets/cr2/a3.jpg";
+import a4 from "../../assets/cr2/a4.jpg";
+import a5 from "../../assets/cr2/a5.jpg";
+import a6 from "../../assets/cr2/a6.jpg";
+import a7 from "../../assets/cr2/a7.jpg";
+import a8 from "../../assets/cr2/a8.jpg";
+import a9 from "../../assets/cr2/a9.jpg";
+import a10 from "../../assets/cr2/a10.jpg";
+import a11 from "../../assets/cr2/a11.jpg";
+import a12 from "../../assets/cr2/a12.jpg";
+import a13 from "../../assets/cr2/a13.jpg";
+import a14 from "../../assets/cr2/a14.jpg";
 
 const About5 = () => {
   return (
@@ -73,26 +72,24 @@ const About5 = () => {
   );
 };
 
-
-
 const Testimonial = () => {
   return (
     <div className="slide">
-      <img src={c1}/>
+      <img src={c1} />
     </div>
   );
 };
 const Testimonial2 = () => {
   return (
     <div className="slide">
-      <img src={c2}/>
+      <img src={c2} />
     </div>
   );
 };
 const Testimonial3 = () => {
   return (
     <div className="slide">
-      <img src={c3}/>
+      <img src={c3} />
     </div>
   );
 };
@@ -100,77 +97,77 @@ const Testimonial3 = () => {
 const Testimonial4 = () => {
   return (
     <div className="slide">
-      <img src={c4}/>
+      <img src={c4} />
     </div>
   );
 };
 const Testimonial5 = () => {
   return (
     <div className="slide">
-      <img src={c5}/>
+      <img src={c5} />
     </div>
   );
 };
 const Testimonial6 = () => {
   return (
     <div className="slide">
-      <img src={c6}/>
+      <img src={c6} />
     </div>
   );
 };
 const Testimonial7 = () => {
   return (
     <div className="slide">
-      <img src={c7}/>
+      <img src={c7} />
     </div>
   );
 };
 const Testimonial8 = () => {
   return (
     <div className="slide">
-      <img src={c8}/>
+      <img src={c8} />
     </div>
   );
 };
 const Testimonial9 = () => {
   return (
     <div className="slide">
-      <img src={c9}/>
+      <img src={c9} />
     </div>
   );
 };
 const Testimonial10 = () => {
   return (
     <div className="slide">
-      <img src={c10}/>
+      <img src={c10} />
     </div>
   );
 };
 const Testimonial11 = () => {
   return (
     <div className="slide">
-      <img src={c11}/>
+      <img src={c11} />
     </div>
   );
 };
 const Testimonial12 = () => {
   return (
     <div className="slide">
-      <img src={c12}/>
+      <img src={c12} />
     </div>
   );
 };
 const Testimonial13 = () => {
   return (
     <div className="slide">
-      <img src={c13}/>
+      <img src={c13} />
     </div>
   );
 };
 const Testimonial14 = () => {
   return (
     <div className="slide">
-      <img src={c14}/>
+      <img src={c14} />
     </div>
   );
 };
@@ -180,7 +177,7 @@ const Testimonial14 = () => {
 const Testimonial15 = () => {
   return (
     <div className="slide">
-      <img src={a1}/>
+      <img src={a1} />
     </div>
   );
 };
@@ -188,7 +185,7 @@ const Testimonial15 = () => {
 const Testimonial16 = () => {
   return (
     <div className="slide">
-      <img src={a2}/>
+      <img src={a2} />
     </div>
   );
 };
@@ -196,7 +193,7 @@ const Testimonial16 = () => {
 const Testimonial17 = () => {
   return (
     <div className="slide">
-      <img src={a3}/>
+      <img src={a3} />
     </div>
   );
 };
@@ -204,7 +201,7 @@ const Testimonial17 = () => {
 const Testimonial18 = () => {
   return (
     <div className="slide">
-      <img src={a4}/>
+      <img src={a4} />
     </div>
   );
 };
@@ -212,7 +209,7 @@ const Testimonial18 = () => {
 const Testimonial19 = () => {
   return (
     <div className="slide">
-      <img src={a5}/>
+      <img src={a5} />
     </div>
   );
 };
@@ -220,7 +217,7 @@ const Testimonial19 = () => {
 const Testimonial20 = () => {
   return (
     <div className="slide">
-      <img src={a6}/>
+      <img src={a6} />
     </div>
   );
 };
@@ -228,7 +225,7 @@ const Testimonial20 = () => {
 const Testimonial21 = () => {
   return (
     <div className="slide">
-      <img src={a7}/>
+      <img src={a7} />
     </div>
   );
 };
@@ -236,7 +233,7 @@ const Testimonial21 = () => {
 const Testimonial22 = () => {
   return (
     <div className="slide">
-      <img src={a8}/>
+      <img src={a8} />
     </div>
   );
 };
@@ -244,7 +241,7 @@ const Testimonial22 = () => {
 const Testimonial23 = () => {
   return (
     <div className="slide">
-      <img src={a9}/>
+      <img src={a9} />
     </div>
   );
 };
@@ -252,7 +249,7 @@ const Testimonial23 = () => {
 const Testimonial24 = () => {
   return (
     <div className="slide">
-      <img src={a10}/>
+      <img src={a10} />
     </div>
   );
 };
@@ -260,7 +257,7 @@ const Testimonial24 = () => {
 const Testimonial25 = () => {
   return (
     <div className="slide">
-      <img src={a11}/>
+      <img src={a11} />
     </div>
   );
 };
@@ -268,7 +265,7 @@ const Testimonial25 = () => {
 const Testimonial26 = () => {
   return (
     <div className="slide">
-      <img src={a12}/>
+      <img src={a12} />
     </div>
   );
 };
@@ -276,7 +273,7 @@ const Testimonial26 = () => {
 const Testimonial27 = () => {
   return (
     <div className="slide">
-      <img src={a13}/>
+      <img src={a13} />
     </div>
   );
 };
@@ -284,7 +281,7 @@ const Testimonial27 = () => {
 const Testimonial28 = () => {
   return (
     <div className="slide">
-      <img src={a14}/>
+      <img src={a14} />
     </div>
   );
 };
