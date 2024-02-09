8e27d2307dd2ce897dd5f8e7eecc87497b9666a9
Anmol Jhamb <talktoanmol@outlook.com>
2024-02-10 01:29:08 +0530

    Fix the link.

--- fest-website-next/src/app/Content.tsx
+++ fest-website-next/src/app/Content.tsx
@@ -31,7 +31,7 @@ function Content() {
         <div className="w-[80vw] mx-auto">
           <div className="grid grid-cols-1 gap-4 lg:grid-cols-2  lg:gap-3">
             <EventsPageGrid
-              url="/events/67milestone"
+              url="/events/67thmilestone"
               logo={MileStone}
               bg={HomeCard1}
             />
