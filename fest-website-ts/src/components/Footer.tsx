532f47b697f861706f802ca6e205fa7d6b262e50
Anmol Jhamb <talktoanmol@outlook.com>
2024-02-09 16:15:34 +0530

    Footer fixed.

--- fest-website-ts/src/components/Footer.tsx
+++ fest-website-ts/src/components/Footer.tsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import footer from "../assets/navbarimg.png";
+import { SocialIcon } from "react-social-icons";
 
 function Footer() {
   return (
@@ -6,7 +8,7 @@ function Footer() {
       <div className="mx-auto max-w-screen-3xl px-4 pb-6 pt-16 xl:ml-7 sm:px-6  lg:pt-24">
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
           <div className="xl:mr-16">
-            <h1 className="text-2xl text-white font-bold mb-4">
+            <h1 className="text-3xl text-white font-bold mb-4">
               67th Milestone Hero's Challenge
             </h1>
             <div className="border  p-4 mx-auto col-span-full  ">
@@ -26,61 +28,58 @@ function Footer() {
                 <li>
                   <a
                     className="text-white transition hover:text-gray-300/75"
-                    href="/"
+                    href="https://bmu.edu.in"
+                    target="_blank"
                   >
-                    FAQ's
+                    BMU
                   </a>
                 </li>
-
                 <li>
-                  <a
-                    className="text-white  transition hover:text-gray-300/75"
-                    href="/"
+                  <Link
+                    className="text-white transition hover:text-gray-300/75"
+                    to="/about"
                   >
-                    Contact Us
-                  </a>
+                    About
+                  </Link>
                 </li>
-
                 <li>
-                  <a
-                    className="text-white  transition hover:text-gray-300/75"
-                    href="/"
+                  <Link
+                    className="text-white transition hover:text-gray-300/75"
+                    to="/events"
                   >
-                    Accomodation
-                  </a>
+                    Events
+                  </Link>
                 </li>
               </ul>
             </div>
             <div className="text-center sm:text-left xl:ml-18">
               <p className="text-3xl font-bold text-white">Social Media</p>
 
-              <ul className="mt-8 flex justify-center gap-6 sm:justify-start md:gap-8">
+              <ul className="mt-8 flex justify-center items-center gap-6 sm:justify-start md:gap-8">
                 <li>
                   <a
-                    href="/"
+                    href="https://www.linkedin.com/in/bml-munjal-university-1990b4152?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
                     rel="noreferrer"
                     target="_blank"
                     className="text-white transition hover:text-gray-400"
                   >
-                    <span className="sr-only">Facebook</span>
+                    <span className="sr-only">LinkedIn</span>
                     <svg
-                      className="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
                       fill="currentColor"
                       viewBox="0 0 24 24"
-                      aria-hidden="true"
+                      className="h-6 w-6"
                     >
-                      <path
-                        fillRule="evenodd"
-                        d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                        clipRule="evenodd"
-                      />
+                      <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                     </svg>
                   </a>
                 </li>
 
                 <li>
                   <a
-                    href="/"
+                    href="https://www.instagram.com/67thmilestone_heroschallenge?igsh=YnE1MHMxa2xrOWh5"
                     rel="noreferrer"
                     target="_blank"
                     className="text-white transition hover:text-gray-400"
@@ -103,20 +102,16 @@ function Footer() {
 
                 <li>
                   <a
-                    href="/"
+                    href="https://x.com/BMLUniv?t=soyuXNbP0H8Kkmaa41bDAg&s=09"
                     rel="noreferrer"
                     target="_blank"
                     className="text-white transition hover:text-gray-400"
                   >
                     <span className="sr-only">Twitter</span>
-                    <svg
-                      className="h-6 w-6"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                      aria-hidden="true"
-                    >
-                      <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                    </svg>
+                    <SocialIcon
+                      className="h-8 w-8"
+                      url="https://x.com/BMLUniv?t=soyuXNbP0H8Kkmaa41bDAg&s=09"
+                    />
                   </a>
                 </li>
               </ul>
