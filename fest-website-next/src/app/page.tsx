31a9a3b94f417ad815e08062300e5eb71f7018c5
Siddhant Sharma <siddhant.sharma.21cse@bmu.edu.in>
2024-02-22 19:58:16 +0530

    changed bizbash reg link and added analytics

--- fest-website-next/src/app/page.tsx
+++ fest-website-next/src/app/page.tsx
@@ -1,8 +1,11 @@
 import Content from "./Content";
 import HeroSection from "./HeroSection";
+import { Analytics } from "@vercel/analytics/react"
+
 export default function Home() {
   return (
     <>
+      <Analytics/>
       <HeroSection />
       <Content />
     </>
